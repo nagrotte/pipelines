357b4f52bf16aa8080796e7f491990b9b49c4c9c
Nag Rotte <nag.rotte@gmail.com>
2024-02-17 16:38:20 -0600

    Initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BackendAPI;
+using Newtonsoft.Json;
+
+namespace BackendAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public ProductController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public IEnumerable<Product> Get()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product 1", Price = 10.99m },
+                new Product { Id = 2, Name = "Product 2", Price = 20.49m }
+            };
+
+            // Serialize products to JSON using Newtonsoft.Json
+            var json = JsonConvert.SerializeObject(products);
+
+            // Return JSON response
+            return products.ToArray();
+
+        
+
+        }
+    }
+}
